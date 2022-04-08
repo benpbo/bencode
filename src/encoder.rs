b59c8ed5bd0c09eef4aaead1b9d5f80c8442ac8b
benpbo <prizeben@gmail.com>
2022-04-08 15:05:24 +0300

    Implement encode_number method

--- src/encoder.rs
+++ src/encoder.rs
@@ -1,6 +1,8 @@
 use crate::bencode::Bencode;
 use std::io::Write;
 
+pub type Result<T> = std::io::Result<T>;
+
 pub struct Encoder<W: Write> {
     writer: W,
 }
@@ -10,7 +12,16 @@ impl<W: Write> Encoder<W> {
         Self { writer }
     }
 
-    pub fn encode(&mut self, _decoded: Bencode) {
-        todo!()
+    pub fn encode(&mut self, decoded: Bencode) -> Result<()> {
+        match decoded {
+            Bencode::Integer(number) => self.encode_number(number),
+            Bencode::String(_bytes) => todo!(),
+            Bencode::List(_list) => todo!(),
+            Bencode::Dictionary(_dictionary) => todo!(),
+        }
+    }
+
+    fn encode_number(&mut self, number: i64) -> Result<()> {
+        write!(&mut self.writer, "i{}e", number)
     }
 }
